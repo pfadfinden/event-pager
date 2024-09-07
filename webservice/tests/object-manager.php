ca6ed3f58a51e4928c09eb01b7638e5cbd957a59
juliusstoerrle <25788444+juliusstoerrle@users.noreply.github.com>
2024-09-07 21:12:20 +0200

    Add phpstan extension utility scripts

---
+++ webservice/tests/object-manager.php
@@ -0,0 +1,14 @@
+/**
+ * This file is used by phpstan/phpstan-doctrine extionsion for DQL validation.
+ */
+
+use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+(new Dotenv())->bootEnv(__DIR__ . '/../.env');
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$kernel->boot();
+return $kernel->getContainer()->get('doctrine')->getManager();
