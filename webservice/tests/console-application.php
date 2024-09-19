c8cf442e30b4afd4ecf9b85467d4855b7a781599
juliusstoerrle <25788444+juliusstoerrle@users.noreply.github.com>
2024-09-19 18:08:22 +0000

    Apply suggestions from code review
    Co-authored-by: Philipp Cordes <pc@irgendware.net>
    Signed-off-by: juliusstoerrle <25788444+juliusstoerrle@users.noreply.github.com>

--- webservice/tests/console-application.php
+++ webservice/tests/console-application.php
@@ -1,14 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
 /**
- * This file is used by phpstan/phpstan-symfony extionsion for Symfony console command validation.
+ * This file is used by phpstan/phpstan-symfony extension for Symfony console command validation.
  */
 
 use App\Kernel;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Dotenv\Dotenv;
 
-require __DIR__ . '/../vendor/autoload.php';
+require dirname(__DIR__).'/vendor/autoload.php';
 
-(new Dotenv())->bootEnv(__DIR__ . '/../.env');
+(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 return new Application($kernel);
--- webservice/tests/object-manager.php
+++ webservice/tests/object-manager.php
@@ -1,14 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
 /**
- * This file is used by phpstan/phpstan-doctrine extionsion for DQL validation.
+ * This file is used by phpstan/phpstan-doctrine extension for DQL validation.
  */
 
 use App\Kernel;
 use Symfony\Component\Dotenv\Dotenv;
 
-require __DIR__ . '/../vendor/autoload.php';
+require dirname(__DIR__).'/vendor/autoload.php';
 
 (new Dotenv())->bootEnv(__DIR__ . '/../.env');
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $kernel->boot();
+
 return $kernel->getContainer()->get('doctrine')->getManager();
