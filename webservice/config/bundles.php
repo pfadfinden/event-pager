cda36f52c501d09a475001b96fa466025abcfd08
Max Nerius <max.nerius@proton.me>
2024-10-19 12:42:29 +0200

    Add symfony/maker-bundle for boilerplate generation

--- webservice/config/bundles.php
+++ webservice/config/bundles.php
@@ -10,4 +10,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
