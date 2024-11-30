bb4934c9c10ecff5605dfa8594c22e5428c5e50b
Philipp Cordes <philipp.cordes@pfadfinden.de>
2024-11-30 11:16:36 +0100

    Fix coding style

--- webservice/tests/console-application.php
+++ webservice/tests/console-application.php
@@ -15,4 +15,5 @@ require dirname(__DIR__).'/vendor/autoload.php';
 (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+
 return new Application($kernel);
