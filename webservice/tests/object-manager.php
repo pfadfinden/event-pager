bd4988f13e2e1a0911eb5c2e6cadd3101f021593
Philipp Cordes <philipp.cordes@pfadfinden.de>
2024-11-30 11:16:36 +0100

    Fix coding style

--- webservice/tests/object-manager.php
+++ webservice/tests/object-manager.php
@@ -11,7 +11,7 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-(new Dotenv())->bootEnv(__DIR__ . '/../.env');
+(new Dotenv())->bootEnv(__DIR__.'/../.env');
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $kernel->boot();
