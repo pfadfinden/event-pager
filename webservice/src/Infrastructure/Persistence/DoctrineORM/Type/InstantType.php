4cd0fdd663fd66720058d7fb93dd5a12dea6ba2b
juliusstoerrle <25788444+juliusstoerrle@users.noreply.github.com>
2025-02-06 20:14:02 +0100

    Adds various improvements around our doctrine usage (#76)
    * Improvements around Doctrine
    
    Add contract for Unit of Work
    Add package for integration tests
    Enable databse integration tests in CI
    Fix folder structure
    Enable phpstan doctrine extension
    
    * Update to docker/bake-action@v6
    
    * Add image tag to have it tagged by buildx and discoverable by compose in CI
    
    * Disable Database Schema Validation
    
    Due to dbal4 issue with custom types

--- webservice/src/Infrastructure/Persistence/DoctrineORM/Type/InstantType.php
+++ webservice/src/Infrastructure/Persistence/DoctrineORM/Type/InstantType.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Infrastructure\Doctrine\DBAL\Type;
+namespace App\Infrastructure\Persistence\DoctrineORM\Type;
 
 use Brick\DateTime\Instant;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
@@ -43,8 +43,6 @@ final class InstantType extends Type
      * @template T
      *
      * @param T $value
-     *
-     * @return (T is null ? null : Instant)
      */
     public function convertToPHPValue($value, AbstractPlatform $platform): ?Instant
     {
