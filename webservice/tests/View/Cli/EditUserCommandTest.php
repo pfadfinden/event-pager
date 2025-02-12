8d05e805efe4b7572d158943642f1e8980ae38fb
MuellerMarlon <marlon130403@gmail.com>
2025-02-12 10:32:50 +0100

    Added functional test

---
+++ webservice/tests/View/Cli/EditUserCommandTest.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\View\Cli;
+
+use App\Core\UserManagement\Model\User;
+use App\View\Cli\EditUserCommand;
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Metadata\CoversClass;
+use PHPUnit\Metadata\Group;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+#[CoversClass(EditUserCommand::class)]
+#[Group('functional')]
+final class EditUserCommandTest extends KernelTestCase
+{
+    public function getEntityManager(): EntityManagerInterface
+    {
+        return self::$kernel->getContainer()->get('doctrine.orm.entity_manager');
+    }
+
+    protected function setUp(): void
+    {
+        self::bootKernel();
+        $em = $this->getEntityManager();
+        $em->persist($this->sampleUser('edituser'));
+        $em->flush();
+        $em->clear();
+    }
+
+    protected function tearDown(): void
+    {
+        $em = $this->getEntityManager();
+        $user = $em->getRepository(User::class)->findOneBy(['username' => 'edituser']);
+        $em->remove($user);
+        $em->flush();
+        $em->clear();
+        self::ensureKernelShutdown();
+    }
+
+    private function sampleUser(string $username): User
+    {
+        $user = new User();
+        $user->setUsername($username);
+        $user->setPassword('');
+        $user->setDisplayname('');
+        $user->setRoles(['ROLE_TEST']);
+
+        return $user;
+    }
+
+    public function testExecuteWithMinimalArguments(): void
+    {
+        $application = new Application(self::$kernel);
+
+        $command = $application->find('app:user:edit');
+        $commandTester = new CommandTester($command);
+        $em = $this->getEntityManager();
+        $user = $em->getRepository(User::class)->findOneBy(['username' => 'edituser']);
+
+        $commandTester->execute([
+            'username' => 'edituser',
+        ]); // no password or display name
+
+        $commandTester->assertCommandIsSuccessful();
+        $result = $em->getRepository(User::class)->findOneBy(['username' => 'edituser']);
+        self::assertInstanceOf(User::class, $result);
+        self::assertEquals($user, $result);
+    }
+
+    public function testExecuteWithAllArguments(): void
+    {
+        $application = new Application(self::$kernel);
+        $command = $application->find('app:user:edit');
+        $commandTester = new CommandTester($command);
+
+        $commandTester->execute([
+            'username' => 'edituser',
+            '--password' => 'newpassword',
+            '--displayname' => 'newdisplayname',
+            '--addRole' => ['ROLE_ADMIN'],
+            '--removeRole' => ['ROLE_TEST'],
+        ]);
+
+        $commandTester->assertCommandIsSuccessful();
+        $em = $this->getEntityManager();
+        $result = $em->getRepository(User::class)->findOneBy(['username' => 'edituser']);
+        self::assertInstanceOf(User::class, $result);
+        self::assertSame('newdisplayname', $result->getDisplayname());
+        self::assertNotSame('', $result->getPassword());
+        self::assertNotSame('newpassword', $result->getPassword());
+        self::assertContains('ROLE_ADMIN', $result->getRoles());
+        self::assertNotContains('ROLE_TEST', $result->getRoles());
+        self::assertCount(2, $result->getRoles());
+    }
+}
