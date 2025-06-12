a5d61ffad22e9ddb3bde864fb210582f8ddecd25
juliusstoerrle <25788444+juliusstoerrle@users.noreply.github.com>
2025-06-12 16:19:01 +0200

    Fix wrong renamings

--- webservice/src/Core/TransportManager/Application/DefaultTransportManager.php
+++ webservice/src/Core/TransportManager/Application/DefaultTransportManager.php
@@ -9,7 +9,7 @@ use App\Core\TransportContract\Port\Transport;
 use App\Core\TransportContract\Port\TransportFactory;
 use App\Core\TransportContract\Port\TransportManager;
 use App\Core\TransportManager\Model\TransportConfiguration;
-use App\Core\TransportManager\Query\AllPager;
+use App\Core\TransportManager\Query\AllTransports;
 use Symfony\Component\DependencyInjection\Attribute\AutowireIterator;
 
 /**
@@ -32,7 +32,7 @@ final readonly class DefaultTransportManager implements TransportManager
     public function activeTransports(): iterable
     {
         /** @var iterable<TransportConfiguration> $enabledTransports */
-        $enabledTransports = $this->queryBus->get(AllPager::thatAreEnabled());
+        $enabledTransports = $this->queryBus->get(AllTransports::thatAreEnabled());
 
         foreach ($enabledTransports as $transportConfig) {
             $transport = $this->instantiate($transportConfig);
