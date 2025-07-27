76f2dfcb086fe3fdec38dc410c9ebb0c8049bf08
juliusstoerrle <25788444+juliusstoerrle@users.noreply.github.com>
2025-07-27 16:30:27 +0200

    Add authorisation checks and role_hierachy
    Co-authored-by: TilDans <htsanders@web.de>

--- webservice/src/View/Web/Admin/SystemInfoComponent.php
+++ webservice/src/View/Web/Admin/SystemInfoComponent.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace App\View\Web\Admin;
 
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 use const PHP_VERSION;
 
 #[AsLiveComponent('SystemInfo', template: 'admin/_component/system_info.html.twig')]
+#[IsGranted('ROLE_ADMIN')]
 class SystemInfoComponent
 {
     use DefaultActionTrait;
