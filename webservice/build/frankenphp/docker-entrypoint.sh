b39d32d2b5f682d9dcafcfa8799b061d9e273e14
juliusstoerrle <25788444+juliusstoerrle@users.noreply.github.com>
2026-01-18 20:38:46 +0100

    Fix & update from upstream symfony-docker

--- webservice/build/frankenphp/docker-entrypoint.sh
+++ webservice/build/frankenphp/docker-entrypoint.sh
@@ -6,6 +6,10 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 		composer install --prefer-dist --no-progress --no-interaction
 	fi
 
+	# Display information about the current project
+	# Or about an error in project initialization
+	php bin/console -V
+
 	if grep -q ^DATABASE_URL= .env; then
 		echo 'Waiting for database to be ready...'
 		ATTEMPTS_LEFT_TO_REACH_DATABASE=60
@@ -20,20 +24,18 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 			echo "Still waiting for database to be ready... Or maybe the database is not reachable. $ATTEMPTS_LEFT_TO_REACH_DATABASE attempts left."
 		done
 
-		if [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ]; then			echo 'The database is not up or not reachable:'
+		if [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ]; then
+			echo 'The database is not up or not reachable:'
 			echo "$DATABASE_ERROR"
 			exit 1
 		else
 			echo 'The database is now ready and reachable'
 		fi
 
-		if [ "$( find ./migrations -iname '*.php' -print -quit )" ]; then
+		if [ "$(find ./migrations -iname '*.php' -print -quit)" ]; then
 			php bin/console doctrine:migrations:migrate --no-interaction --all-or-nothing
 		fi
 	fi
-
-	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
-	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
 
 	echo 'PHP app ready!'
 fi
